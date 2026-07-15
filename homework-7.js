ae90b19d0e660c472947d80e235ce9c958d28479
Mahomet123 <mahomet1994@gmail.com>
2026-07-15 23:27:59 +0300

    fix

--- homework-7.js
+++ homework-7.js
@@ -1,7 +1,7 @@
 // Задание 3
 let city = 'Грозный';
 let temperature = 30;
-const tempCity = `Сейчас в городе ${city} температура - ${temperature} градусов по Цельсию`;
+const tempCity = `Сейчас в ${city} температура - ${temperature} градусов по Цельсию`;
 console.log(tempCity);
 
 // Задание 4
@@ -14,47 +14,36 @@ function logLightSpeed(speed) {
     console.log('Скорость света');
   }  else {
     console.log('Субсветовая скорость');
-}};
+  }
+}
 
 logLightSpeed(399792458)    // Сверхсветовая скорость
 logLightSpeed(199792458)    // Субсветовая скорость
 logLightSpeed(299792458)    //Скорость света
 
-// Задание 4 с использованием терального оператора
-//Скорость света берем из константа выше и далее выполняем функцию
-function logLightSpeed2 (speed2) {
-  console.log(speed2);
-  console.log(
-    speed2 > LIGHT_SPEED ? 'Сверхсветовая скорость':
-    speed2 === LIGHT_SPEED ? 'Скорость света':
-    'Субсветовая скорость'
-)};
-
-logLightSpeed2(399792458)    // Сверхсветовая скорость
-logLightSpeed2(199792458)    // Субсветовая скорость
-logLightSpeed2(299792458)    //Скорость света
-
 // Задание 5
 let apple = 'Яблоки Голден';
 let buyProduct = 150;
 
-function budget (calculation) {
+function budget(calculation) {
   if (buyProduct <= calculation) {
-    console.log('Товар', apple, 'приобретён. Спасибо за покупку!');
-  } else {console.log('Вам не хватает', (buyProduct - calculation) + '$ пополните баланс')
-}};
+    console.log(`Товар ${apple} приобретён. Спасибо за покупку!`);
+  } else {
+    console.log(`Вам не хватает ${buyProduct} - ${calculation}) + $ пополните баланс`);
+  }
+}
 
 budget (200);
 budget (100);
 
 // Задание 6
-function myName (name) {
+function myName(name) {
   console.log(name);
   return name;
 }
 myName ("Мухаммад");
 
 // Задание 7
-const myName = Мухаммад
+const myLastName = 'Усманов'
 let age = 32
-let city = Грозный
+let currentCity = 'Грозный'
