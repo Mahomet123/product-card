35842c252530900dfb4b806b1f89427d8dad637b
Mahomet123 <Mahomet1994@gmail.com>
2026-09-03 23:43:15 +0300

    update homework

--- homework-10.js
+++ homework-10.js
@@ -38,32 +38,57 @@ console.log(allProducts)
 Важно: Визуально карточки не должны измениться — всё должно выглядеть так же, как и раньше.
  */
 
-let userInput = prompt('Сколько карточек отобразить? От 1 до 5', [1]);
-console.log(userInput);
+function getCardCount() {
+  let userInput = prompt('Сколько карточек отобразить? От 1 до 5', [1]);
+  console.log(userInput);
+  if (userInput === null) {
+    return null;  // пользователь отменил
+  }
+  if (userInput === '0') {  //если не оборачивать в ковычки не срабатывало
+    alert('0 не принимается!');
+    return getCardCount();  // пользователь ввел 0 вызываем ф-ию снова
+  }
+  if (userInput === "") {
+    alert('Нельзя оставлять пустым!');
+    return getCardCount();
+  }
 
-const quantity = Number(userInput);
-console.log(Number.isNaN(quantity));
-if (isNaN(quantity) || quantity < 1 || quantity > 100) {
-  alert('Введите правильное число!')
+  const quantity = Number(userInput);  //преобразуем в число (все что принимает prompt форматируется в строку, даже числа)
+  console.log(Number.isNaN(quantity));
+
+  if (isNaN(quantity) || quantity < 1 || quantity > 5) {  //проверяем получили ли число и в диапазоне ли он от 0-5
+    alert('Введите число от 1 - 5!');
+    return getCardCount();
+  } else {
+    return quantity;
+  }
+
+
 }
-console.log(quantity);
+const result = getCardCount();
+console.log(result);
 
+// Рендерим карточки
 const cardTemplate = document.getElementById('product-card-template');
 const productList = document.querySelector('.products-list');
-productCards.slice(0, quantity).forEach((card) => {
-  const productClone = cardTemplate.content.cloneNode(true);
-  productClone.querySelector('.product-card__img').src = card.img
-  productClone.querySelector('.product-card__label').textContent = card.label
-  productClone.querySelector('.product-card__title').textContent = card.title
-  productClone.querySelector('.product-card__description').textContent = card.description
-  productClone.querySelector('.product-card__price span').textContent = card.price
 
-  const componentList = productClone.querySelector('.product-card__component-list');
-  card.components.forEach((component) => {
-    const li = document.createElement('li');
-    li.textContent = component
-    componentList.appendChild(li);
-  })
-  productList.appendChild(productClone);
-  console.log(productClone);
-});
+function renderCards() {
+  const rendering = productCards.slice(0, result).forEach((card) => {
+    const productClone = cardTemplate.content.cloneNode(true);
+    productClone.querySelector('.product-card__img').src = card.img
+    productClone.querySelector('.product-card__label').textContent = card.label
+    productClone.querySelector('.product-card__title').textContent = card.title
+    productClone.querySelector('.product-card__description').textContent = card.description
+    productClone.querySelector('.product-card__price span').textContent = card.price
+
+    const componentList = productClone.querySelector('.product-card__component-list');
+    card.components.forEach((component) => {
+      const li = document.createElement('li');
+      li.textContent = component
+      componentList.appendChild(li);
+    })
+    productList.appendChild(productClone);
+    console.log(productClone);
+  });
+}
+const render = renderCards();
